d8ef83c49f57ae6516f3020d772f98f4793af6f8
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-03-20 16:09:08 +0700

    Makes the middle header sticky

---
+++ source/index.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  handleSticky();
+};
+
+const handleSticky = () => {
+  // Get the header
+  const header = document.getElementById('sticky-header');
+
+  // Get the offset position of the navbar
+  const sticky = header.offsetTop;
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  if (window.pageYOffset > sticky) {
+    header.classList.add('sticky-header');
+  } else {
+    header.classList.remove('sticky-header');
+  }
+};
