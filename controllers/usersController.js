e72f824ac53b262c3ae7d9c5a41874b89f31dc2a
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-07-06 21:22:48 +0700

    Add log out and edit feature

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,19 @@
-'use strict';
+"use strict";
 
 const controller = {};
-const models = require('../models');
+const models = require("../models");
+
+controller.edit = async (req, res) => {
+  await models.User.update(
+    {
+      name: req.body.nickname,
+    },
+    {
+      where: { id: req.user.id },
+    }
+  );
+
+  res.redirect("/users/my-account");
+};
 
 module.exports = controller;
